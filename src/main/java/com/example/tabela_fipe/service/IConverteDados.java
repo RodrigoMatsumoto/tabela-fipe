1bd69ec5b613568a3d9894c850dcd58065a49172
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-05-31 23:01:56 -0300

    obter lista

--- src/main/java/com/example/tabela_fipe/service/IConverteDados.java
+++ src/main/java/com/example/tabela_fipe/service/IConverteDados.java
@@ -1,6 +1,10 @@
 package com.example.tabela_fipe.service;
 
+import java.util.List;
+
 public interface IConverteDados {
 
   <T> T obterDados(String json, Class<T> classe);
+
+  <T> List<T> obterLista(String json, Class<T> classe);
 }
